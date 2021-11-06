224155c9c275affa0a8a3d0f19b29b89e0e64687
Anthony Byuraev <anthony.byuraev@gmail.com>
2021-11-06 20:46:19 +0300

    develop interp1d with map

--- develop/core/interp.hpp
+++ develop/core/interp.hpp
@@ -1,43 +1,55 @@
 #ifndef _INTERP_H_
 #define _INTERP_H_
 
+#include <map>
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 using vector_t = std::vector<float>;
-using matrix_t = std::vector<
-    std::vector<float>
->;
-using pospair_t = std::pair<int, int>;
-using boundpair_t = std::pair<
+using vector_i = std::pair<
     std::vector<float>::const_iterator,
     std::vector<float>::const_iterator
 >;
+using matrix_t = std::vector<
+    std::vector<float>
+>;
+using pos_t = std::pair<
+    int,
+    int
+>;
+using map_t = std::map<
+    float,
+    float
+>;
+
+float interp1d(
+    const float,
+    const vector_t&,
+    const vector_t&);
+float interp1d(
+    const float,
+    const map_t&);
 
 #ifdef  __cplusplus
 extern "C" {
 #endif
 
-float testInterp1d();
-float testInterp2d();
+int testInterp1d();
+int testInterp2d();
 float interp(
     const float,
     const float*,
     const float*);
-float interp1d(
-    const float&,
-    const vector_t&,
-    const vector_t&);
 float interp2d(
     const float,
     const float,
     const vector_t&,
     const vector_t&,
     const matrix_t&);
-pospair_t findPosition_(
+pos_t findPosition_(
     const vector_t&,
-    const boundpair_t&);
+    const vector_i&);
 
 #ifdef  __cplusplus
 }
--- develop/core/interp.py
+++ develop/core/interp.py
@@ -4,10 +4,10 @@ import ctypes
 libinterp = ctypes.CDLL('./interp.so', winmode=0)
 
 libinterp.testInterp1d.argtypes = [ctypes.c_void_p]
-libinterp.testInterp1d.restype = ctypes.c_float
+libinterp.testInterp1d.restype = ctypes.c_int
 
 libinterp.testInterp2d.argtypes = [ctypes.c_void_p]
-libinterp.testInterp2d.restype = ctypes.c_float
+libinterp.testInterp2d.restype = ctypes.c_int
 
 print(libinterp.testInterp1d(None))
 print(libinterp.testInterp2d(None))
